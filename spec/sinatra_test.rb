11407ed2b4e5a41084f9f3e36691dbfbe7d19bde
Bryan Shelton <bryan@sheltonplace.com>
2011-11-16 12:21:53 -0600

    Quick sinatra app in spec for testing user agent from a browser

---
+++ spec/sinatra_test.rb
@@ -0,0 +1,10 @@
+$:.unshift File.expand_path('../../lib',__FILE__)
+require 'kurogo_device_detection'
+require 'sinatra'
+require 'yaml'
+
+# Display the kurogo data based on the user agent
+get '/' do
+  k = KurogoDeviceDetection::Device.new(:user_agent => request.env['HTTP_USER_AGENT'])
+  "<pre>User Agent: #{YAML::dump k.detect}</pre>"
+end
